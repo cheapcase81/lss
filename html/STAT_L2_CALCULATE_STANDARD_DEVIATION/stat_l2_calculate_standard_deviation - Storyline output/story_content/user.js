97ce72b20f993157e574016255ade3d03bc51206
cheapcase81 <109812841+cheapcase81@users.noreply.github.com>
2024-03-22 20:43:40 +0530

    Add files via upload

---
+++ html/STAT_L2_CALCULATE_STANDARD_DEVIATION/stat_l2_calculate_standard_deviation - Storyline output/story_content/user.js
@@ -0,0 +1,17 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6Y1d1ytd8zk":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  SCORM_SetCompleted();
+SCORM_CommitData();
+ExecFinish("Finish");
+}
+

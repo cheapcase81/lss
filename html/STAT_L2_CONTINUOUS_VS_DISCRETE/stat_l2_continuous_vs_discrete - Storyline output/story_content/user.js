66e5d89335c89f33dd25f57aa1189468dd569bba
cheapcase81 <109812841+cheapcase81@users.noreply.github.com>
2024-03-22 20:45:24 +0530

    Add files via upload

---
+++ html/STAT_L2_CONTINUOUS_VS_DISCRETE/stat_l2_continuous_vs_discrete - Storyline output/story_content/user.js
@@ -0,0 +1,17 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "64VibLi5cZ5":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  SCORM_SetCompleted();
+SCORM_CommitData();
+ExecFinish("Finish");
+}
+

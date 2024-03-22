96dfeb3e963159f1be49b0c9303c4b87966738e4
cheapcase81 <109812841+cheapcase81@users.noreply.github.com>
2024-03-22 20:50:29 +0530

    Add files via upload

---
+++ html/STAT_L2_SAMPLING_PROCESS/stat_l2_sampling_process - Storyline output/story_content/user.js
@@ -0,0 +1,17 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6EJXnWywjNw":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  SCORM_SetCompleted();
+SCORM_CommitData();
+ExecFinish("Finish");
+}
+

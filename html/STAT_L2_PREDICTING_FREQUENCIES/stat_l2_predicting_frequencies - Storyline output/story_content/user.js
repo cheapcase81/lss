6b417406a92c6ae103943bba0bdf7beddecee2f9
cheapcase81 <109812841+cheapcase81@users.noreply.github.com>
2024-03-22 20:48:39 +0530

    Add files via upload

---
+++ html/STAT_L2_PREDICTING_FREQUENCIES/stat_l2_predicting_frequencies - Storyline output/story_content/user.js
@@ -0,0 +1,27 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "69sYJCPpTbh":
+        Script1();
+        break;
+      case "5mzDUTgyIY4":
+        Script2();
+        break;
+  }
+}
+
+function Script1()
+{
+  SCORM_SetCompleted();
+SCORM_CommitData();
+ExecFinish("Finish");
+}
+
+function Script2()
+{
+  SCORM_SetCompleted()
+SCORM_CommitData();
+ExecFinish("Finish");
+}
+

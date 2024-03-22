dd9103c79d8a8f19c54290135204a98ecb14b9d7
cheapcase81 <109812841+cheapcase81@users.noreply.github.com>
2024-03-22 20:01:54 +0530

    Add files via upload

---
+++ html/STAT_L2_17_MT_DPMO_TI/stat_l2_17_mt_dpmo_ti - Storyline output/story_content/user.js
@@ -0,0 +1,17 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6Yi1LbkAgQ8":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  SCORM_SetCompleted();
+SCORM_CommitData();
+ExecFinish("Finish");
+}
+

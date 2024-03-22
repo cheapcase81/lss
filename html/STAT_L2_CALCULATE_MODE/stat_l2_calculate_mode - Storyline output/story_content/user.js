9b99f3657698915e635f24b7d2b0670cc856e27b
cheapcase81 <109812841+cheapcase81@users.noreply.github.com>
2024-03-22 20:42:08 +0530

    Add files via upload

---
+++ html/STAT_L2_CALCULATE_MODE/stat_l2_calculate_mode - Storyline output/story_content/user.js
@@ -0,0 +1,17 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6XDbQJd7tiw":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  SCORM_SetCompleted();
+SCORM_CommitData();
+ExecFinish("Finish");
+}
+
